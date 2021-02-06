e63c1be9d01a5b452963a967ede5e0be6be3fcb5
ibnutriyuono <ibnutriyuono@gmail.com>
2021-02-06 22:22:33 +0700

    add: product card component

--- src/components/Main/ProductList.jsx
+++ src/components/Main/ProductList.jsx
@@ -3,9 +3,16 @@ import ProductCard from '../../components/Main/ProductCard'
 
 function ProductList() {
   return (
-    <div>
-      <h1>Product List</h1>
-      <ProductCard />
+    <div className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          {
+            [...Array(8)].map((el, i) => {
+              return <ProductCard key={i}/>
+            })
+          }         
+        </div>
+      </div>
     </div>
   )
 }
