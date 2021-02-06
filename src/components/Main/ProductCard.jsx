abf68a49df237fd7b461292df8375dd68b4a364d
ibnutriyuono <ibnutriyuono@gmail.com>
2021-02-06 23:38:48 +0700

    add: product card

--- src/components/Main/ProductCard.jsx
+++ src/components/Main/ProductCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 function ProductCard() {
+  const history = useHistory()
   return (
-    <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+    <div 
+      className="lg:w-1/4 md:w-1/2 p-4 w-full cursor-pointer"
+      onClick={() => {
+        history.push(`/products/2`)
+      }}>
       <a className="block relative h-48 rounded overflow-hidden">
         <img alt="ecommerce" className="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260" />
       </a>
